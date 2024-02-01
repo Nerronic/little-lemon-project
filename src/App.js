8f482a045c30f52bdcb58162caefe24fd2dbb218
Ty <tyrellcarlton@gmail.com>
2024-02-01 11:02:19 -0800

    Added header, nav, main and Footer components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <div className="App">
+    <>
+   <Header/>
+  <Nav/>
+   <Main/>
+   <Footer/>
+    
       "Homepage"
-    </div>
+   </>
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+        <p>Doormat Navigation</p>
+     <ul>  
+     <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/menu">Menu</a></li>
+        <li><a href="/reservations">Reservations</a></li>
+        <li><a href="/order-online">Order Online</a></li>
+        <li><a href="/login">Login</a></li>
+</ul>
+<p>Contact</p>
+<ul>
+    <li><a href= "/googe.com/maps"> Address</a></li>
+    <li><a href= "/googe.com/maps"> phone number</a></li>
+    <li><a mailto= "abc123@gmail.com">email</a></li>
+</ul>
+
+<p>Social Media Links</p>
+<ul>
+    <li><a href= "/googe.com/maps"> Address</a></li>
+    <li><a href= "/googe.com/maps"> phone number</a></li>
+    <li><a mailto= "abc123@gmail.com">email</a></li>
+</ul>
+
+
+    </footer>
+
+  );
+}
+
+export default Footer;
