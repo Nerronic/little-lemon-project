4da889dce1de56bb1a21e5e0b644e8d41b2c9560
Ty <tyrellcarlton@gmail.com>
2024-02-02 10:01:39 -0800

    made changes to index.htrml, more change to follow

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <img src="/bruchetta" alt=""> </img>
+      <img src="/google.com" alt=""> </img>
         <p>Doormat Navigation</p>
      <ul>  
      <li><a href="/">Home</a></li>
